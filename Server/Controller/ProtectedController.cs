7fe11305e49c46ee72494d0628443cd87657ff32
George-leach <70960013+George-leach@users.noreply.github.com>
2020-09-08 15:47:17 +0100

    Add files via upload

---
+++ Server/Controller/ProtectedController.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using CoreExtensions;
+using DistSysACW.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace DistSysACW.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class BaseClass
+    {
+        private static RSACryptoServiceProvider RsaBase;
+        public BaseClass()
+        {
+            RsaBase = new RSACryptoServiceProvider();
+            if (GetKeyFromContainer("Key") != null)
+            {
+                RsaBase.FromXmlStringCore22(GetKeyFromContainer("Key"));
+            }
+            else
+            {
+                GenKey_SaveInContainer("Key");
+            }
+        
+       
+        }
+        public RSACryptoServiceProvider GetRSA
+        {
+            get
+            {
+                RsaBase.FromXmlStringCore22(GetKeyFromContainer("Key"));
+                return RsaBase;
+            }
+        }
+        public string GetPublicKey {
+            get {
+                return RsaBase.ToXmlStringCore22(false);
+                    }
+        }
+        public static void GenKey_SaveInContainer(string ContainerName)
+        {
+            // Create the CspParameters object and set the key container
+            // name used to store the RSA key pair.  
+            CspParameters cp = new CspParameters();
+            cp.KeyContainerName = ContainerName;
+
+            // Create a new instance of RSACryptoServiceProvider that accesses  
+            // the key container MyKeyContainerName.  
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cp);
+
+            // Display the key information to the console.  
+            Console.WriteLine("Key added to container: \n  {0}", rsa.ToXmlStringCore22(true));
+        }
+        public string  GetKeyFromContainer(string ContainerName)
+        {
+            // Create the CspParameters object and set the key container
+            // name used to store the RSA key pair.  
+            CspParameters cp = new CspParameters();
+            cp.KeyContainerName = ContainerName;
+
+            // Create a new instance of RSACryptoServiceProvider that accesses  
+            // the key container MyKeyContainerName.  
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cp);
+
+            // Display the key information to the console.  
+            return rsa.ToXmlStringCore22(true);
+        }
+        public string GetPrivateKey
+        {
+            get
+            {
+                return RsaBase.ToXmlStringCore22(true);
+            }
+        }
+  
+
+    };
+
+
+    
+  
+
+
+    public class ProtectedController : BaseController
+    {
+      
+     
+        public ProtectedController(UserContext context) : base(context) { }
+        [ActionName("Hello")]
+        public string Hello([FromHeader] string ApiKey)
+        {
+            using (var ctx = new UserContext())
+            {
+                User user1 = ctx.Users.FirstOrDefault(s => s.ApiKey == ApiKey);
+                this.Response.StatusCode = 200;
+                return ("Hello " + user1.Username);
+            }
+    
+        }
+
+        [ActionName("sha1")]
+        public string sha1([FromQuery]string Message)
+        {
+            if (Message != null)
+            {
+                using (SHA1Managed sha1 = new SHA1Managed())
+                {
+                    var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(Message));
+                    var sb = new StringBuilder(hash.Length * 2);
+
+                    foreach (byte b in hash)
+                    {
+                        sb.Append(b.ToString("X2"));
+                    }
+                    this.Response.StatusCode = 200;
+                    return sb.ToString();
+                }
+
+            }
+            else
+            {
+                this.Response.StatusCode = 400;
+                return ("Bad Request");
+            }
+        }
+
+        [ActionName("sha256")]
+        public string sha256([FromQuery]string Message)
+        {
+            if (Message != null)
+            {
+                using (SHA256 sha256 = new SHA256Managed())
+                {
+                    var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(Message));
+                    var sb = new StringBuilder(hash.Length * 2);
+
+                    foreach (byte b in hash)
+                    {
+                        sb.Append(b.ToString("X2"));
+                    }
+                    this.Response.StatusCode = 200;
+                    return sb.ToString();
+                }
+            }
+            else
+            {
+                this.Response.StatusCode = 400;
+                return ("Bad Request");
+            }
+
+        }
+        [ActionName("getpublickey")]
+        public string Pubkey([FromHeader]string ApiKey)
+        {
+            using (var ctx = new UserContext())
+            {
+                User user1 = ctx.Users.FirstOrDefault(s => s.ApiKey == ApiKey);
+
+                if (user1 != null)
+                {
+                    var rsa = new BaseClass();
+                    
+
+                        string _publickey = rsa.GetPublicKey;
+
+                    //var sw = new StringWriter();
+                    // var xs = new XmlSerializer(typeof(RSAParameters));
+                    //  xs.Serialize(sw, _publickey);
+                    Console.WriteLine(_publickey);
+                    return _publickey;
+
+                  
+                    }
+                else
+                {
+                    return "Couldn’t Get the Public Key";
+                }
+            }
+        }
+        [ActionName("sign")]
+        public string Sign([FromHeader]string ApiKey,[FromQuery] string message)
+        {
+            using (var ctx = new UserContext())
+            {
+                User user1 = ctx.Users.FirstOrDefault(s => s.ApiKey == ApiKey);
+
+                if (user1 != null)
+                {
+
+
+                    var rsa = new BaseClass();
+
+                    
+                    var converter = new ASCIIEncoding();
+                    byte[] plainText = converter.GetBytes(message);
+
+                    var rsaWrite = rsa.GetRSA;
+                    var privateParams = rsaWrite.ExportParameters(true); ;
+                    
+
+                    byte[] signature =  rsaWrite.SignData(plainText, new SHA1CryptoServiceProvider());
+                    
+                    return BitConverter.ToString(signature);
+
+                }
+                else
+                {
+                    return "Couldn’t Get the Public Key";
+                }
+            }
+        }
+        static string ByteArrayToHexString(byte[] byteArray)
+        { string hexString = ""; 
+            if (null != byteArray) 
+            { 
+                foreach (byte b in byteArray)
+                { 
+                    hexString += b.ToString("x2"); 
+                } 
+            } return hexString; }
+        static public byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo)
+        { 
+            try
+            {
+                byte[] encryptedData; using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider()) 
+                { 
+                    RSA.ImportParameters(RSAKeyInfo); encryptedData = RSA.Encrypt(DataToEncrypt, false);
+                }
+                return encryptedData;
+            } 
+            catch (CryptographicException e) {
+                Console.WriteLine(e.Message); return null;
+            }
+        }
+    }
+}
---
+++ Server/Controller/UserController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+using DistSysACW.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DistSysACW.Controllers
+{
+ 
+    public class UserController : BaseController    
+    {
+
+        /// <summary>
+        /// Constructs a TalkBack controller, taking the UserContext through dependency injection
+        /// </summary>
+        /// <param name="context">DbContext set as a service in Startup.cs and dependency injected</param>
+        public UserController(UserContext context) : base(context) { }
+   
+        [HttpGet]
+        public string New([FromQuery]string username)
+        {
+            using (var ctx = new UserContext())
+            {
+                return UserDatabaseAccess.CheckUser(ctx, username) == true
+                ? "True - User Does Exist! Did you mean to do a POST to create a new user?"
+                : "False - User Does Not Exist! Did you mean to do a POST to create a new user?";
+            }
+
+        }
+        [HttpPost]
+        [ActionName("new")]
+        public IActionResult PostAction([FromBody] string username)
+        {
+
+            using (var ctx = new UserContext())
+            {
+                if (UserDatabaseAccess.CheckUser(ctx,username) == true)
+                {
+                    this.Response.StatusCode = 403;
+                    return new ObjectResult("Oops. This username is already in use. Please try again with a new username.");
+                }
+                else if (username == null)
+                {
+                    this.Response.StatusCode = 400;
+                    return new ObjectResult("Oops. Make sure your body contains a string with your username and your Content-Type is Content-Type:application/json");
+                }
+                else
+                {
+                    string ret = UserDatabaseAccess.CreateNewUser(ctx, username);
+                    return Ok(ret);
+                }
+            }
+
+
+
+        }
+        [HttpDelete]
+        [ActionName("RemoveUser")]
+        [Authorize(Roles = "Admin,User")]
+        public Boolean Remove([FromHeader] string ApiKey,[FromQuery] string username)
+        {
+
+            using (var ctx = new UserContext())
+            {
+               User  user1 = ctx.Users.FirstOrDefault(s => s.Username == username);
+                if (user1.ApiKey == ApiKey)
+                {
+
+                    UserDatabaseAccess.RemoveUser(ctx,user1);
+                    this.Response.StatusCode = 200;
+                    return (true);
+                }
+                else
+                {
+                    this.Response.StatusCode = 200;
+                    return (false);
+                }
+         
+                  
+            }
+         }
+        [HttpPost]
+        [ActionName("ChangeRole")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult Change([FromBody]JObject values)
+        {
+            string username = (string)values["username"];
+            string role = (string)values["role"];
+            try {
+                using (var ctx = new UserContext())
+                {
+                    if (role.ToLower() != "user" && role.ToLower() != "admin")
+                    {
+                        return BadRequest("NOT DONE: Role does not exist");
+                    }
+                    else if (UserDatabaseAccess.CheckUser(ctx, username) == false)
+                    {
+                        return BadRequest("NOT DONE: Username does not exist");
+                    }
+                    else
+                    {
+                        UserDatabaseAccess.ChangeRole(ctx, username, role);
+                        return Ok("DONE");
+                    }
+                }
+            }
+            catch
+            {
+                return BadRequest("NOT DONE: An error occured");
+            }
+        }
+
+
+
+    }
+}
+
